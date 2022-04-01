9b63cd040430e0bf5f99676036d847c9e9a5a84b
guitarman018 <nzguitarman.programming@gmail.com>
2022-04-01 15:53:44 +1300

    Final product - no alpha

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const panel = document.querySelector('#sketch');
+const btnReset = document.querySelector('#reset')
+btnReset.addEventListener('click', rebuild)
 
 let wide = window.innerWidth;
 let high = window.innerHeight;
 
-let squares = 80
+let squares = 16
 
 function updateSize() {
     
@@ -20,6 +22,7 @@ function updateSize() {
 
 function hoverChange(e) {
   const target = document.querySelector(`#${e.target.id}`)
+  // target.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
   target.style.backgroundColor = 'black';
 }
 
@@ -27,28 +30,36 @@ function hoverChange(e) {
 window.onresize = updateSize;
 window.onload = updateSize;
 
-// Create all of the div elements to etch.  ID structure: etch[x]-[y]
+function rebuild() {
+  let num = prompt('How many squares on each row? Max is 100');
 
-for (let x = 0; x < squares; x++) {
-  
-  for (let y = 0; y < squares; y++) {
-
-    const addMe = document.createElement('div')
-    addMe.setAttribute('id', `etch${x}-${y}`)
-    addMe.style.cssText = `width: ${100/squares}%; height: ${100/squares}%; border-bottom: 1px solid black; border-right: 1px solid black;`
-    if (y == squares - 1 && x != squares - 1) {
-      addMe.style.borderRight = null;
-    } else if (x == squares - 1 && y != squares - 1) {
-      addMe.style.borderBottom = null;
-    } else if (x == squares - 1 && y == squares - 1) {
-      addMe.style.borderRight = null;
-      addMe.style.borderBottom = null;
-    }
-    addMe.addEventListener('mouseover', hoverChange)
-    panel.appendChild(addMe);
-
-
+  while (num > 100 || num < 0) {
+      
+    num = prompt("Try again, how many squares under 100?")
   }
-
+  squares = num;
+  panel.textContent = '';
+  build();
+}
+// Create all of the div elements to etch.  ID structure: etch[x]-[y]
+function build () {
+  for (let x = 0; x < squares; x++) {
+    for (let y = 0; y < squares; y++) {
+      const addMe = document.createElement('div')
+      addMe.setAttribute('id', `etch${x}-${y}`)
+      addMe.style.cssText = `width: ${100/squares}%; height: ${100/squares}%; border-bottom: 1px solid black; border-right: 1px solid black;`
+      if (y == squares - 1 && x != squares - 1) {
+        addMe.style.borderRight = null;
+      } else if (x == squares - 1 && y != squares - 1) {
+        addMe.style.borderBottom = null;
+      } else if (x == squares - 1 && y == squares - 1) {
+        addMe.style.borderRight = null;
+        addMe.style.borderBottom = null;
+      }
+      addMe.addEventListener('mouseover', hoverChange)
+      panel.appendChild(addMe);
+    }
+  }
 }
 
+build();
